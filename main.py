1f0680ae31a65c81b1b624f2e8490f35c2e4f153
Wooft <wooftyaa@gmail.com>
2022-09-16 14:22:29 +0300

    1,2 Done

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def OpenFile():
     return contacts_list
 
 
-def get_names():
+def get_names(): ##Помещение ФИО в соответствующие поля
     pattern = r'([А-ЯЁ]\w+)'
     result = []
     data = OpenFile()
@@ -32,17 +32,35 @@ def get_names():
     return result
 
 
-def get_numbers():
-    pattern = r'(\+7|8)?(\s*)?(\()?(\d\d\d)(\))?\s*(\d\d\d)[-\s]*(\d\d)[-\s]*(\d\d)\s*(\()?(\доб.)?\s*(\d*)(\))?'
+def get_numbers(): ##Исправление номеров телефона
+    data  = get_names()
+    result = []
+    pattern = r'(\+7|8)?(\s*)?(\()?(\d\d\d)(\))?[-\s*]?(\d\d\d)[-\s]*(\d\d)[-\s]*(\d\d)\s*(\()?(\доб.)?\s*(\d*)(\))?'
     suns = r'+7(\4)\6-\7-\8 \10 \11'
+    for  lists in data:
+        for elements in lists:
+            number = re.findall(pattern, elements)
+            if len(number) > 0:
+                num_res = re.sub(pattern, suns, elements)
+                lists.pop(-2)
+                lists.insert(-1, num_res)
+            else:
+                pass
+    return data
 
+def del_duplicate():
+    data = get_numbers()
+    for lists in data:
+        print(lists)
+        if lists[0] and lists[1]:
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
-# with open("phonebook.csv", "w") as f:
-#   datawriter = csv.writer(f, delimiter=',')
-#   # Вместо contacts_list подставьте свой список
-#   datawriter.writerows(contacts_list)
+
 
 if __name__ == "__main__":
-   pprint(get_names())
+   del_duplicate()
+   # with open("phonebook.csv", "w", encoding='UTF-8') as f:
+   #     datawriter = csv.writer(f, delimiter=',')
+   #     # Вместо contacts_list подставьте свой список
+   #     datawriter.writerows(get_numbers())
